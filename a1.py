1da2847d56116429d638286aa60cc3b9a88eb14e
emmavedock <emma.vedock@gmail.com>
2016-09-18 11:57:01 -0400

    Most updated files

--- a1.py
+++ a1.py
@@ -1,12 +1,12 @@
 #a1.py
-#Emma Vedock-Gross ev225
-#6/8/2016
+#Emma Vedock-Gross ev225 Trey Aguirre tea42
+#9/17/2016
 
 """Module for currency exchange
-
 This module provides several string parsing functions to implement a 
 simple currency exchange routine using an online currency service. 
 The primary function in this module is exchange."""
+
 
 #Part A
 def before_space(s):
@@ -24,9 +24,10 @@ def after_space(s):
     space=s.index(' ')
     result=s[space+1:]
     return result
+  
     
 #Part B
-#def first_inside_quotes(s):
+def first_inside_quotes(s):
     """Returns: The first substring of s between two (double) quote characters
     A quote character is one that is inside a string, not one that delimits it.
     We typically use single quotes (') to delimit a string if we want to use a double quote character (") inside of it.
@@ -34,32 +35,100 @@ def after_space(s):
     Example: If s is 'A "B C" D "E F" G', this function still returns 'B C' because it only picks the first such substring.
     Parameter s: a string to search
     Precondition: s a string with at least two (double) quote characters."""
+    start=s.index('"')
+    tail=s[start+1:]
+    end=tail.index('"')
+    result=tail[:end]
+    return result
+
+
+def get_from(json):
+    """Returns: The FROM value in the response to a currency query.
+    Given a JSON response to a currency query, this returns the string inside double quotes (")
+    immediately following the keyword "from". For example, if the JSON is
+    '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
+    then this function returns '2 United States Dollars' (not '"2 United States Dollars"').
+    It returns the empty string if the JSON is the result of on invalid query.
+    Parameter json: a json string to parse
+    Precondition: json is the response to a currency query"""
+    return first_inside_quotes(json[json.index('"from"')+6:])
+
+
+def get_to(json):
+    """Returns: The TO value in the response to a currency query.
+    Given a JSON response to a currency query, this returns the string inside double quotes (")
+    immediately following the keyword "to". For example, if the JSON is
+    '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
+    then this function returns '1.825936 Euros' (not '"1.825936 Euros"').
+    It returns the empty string if the JSON is the result of on invalid query.
+    Parameter json: a json string to parse
+    Precondition: json is the response to a currency query"""    
+    return first_inside_quotes(json[json.index('"to"')+4:])
+
+
+def has_error(json):
+    """Returns: True if the query has an error; False otherwise.
+    Given a JSON response to a currency query, this returns the opposite of the value following the keyword "valid". For example, if the JSON is
+    '{"from":"","to":"","success":false,"error":"Source currency code is invalid."}'
+    then the query is not valid, so this function returns True (It does NOT return the message 'Source currency code is invalid').
+    Parameter json: a json string to parse
+    Precondition: json is the response to a currency query"""
+    start=json.index('success')
+    end=json.index('"error"')
+    result=json[start+9:end-1]
+    return result == 'false'
+
+
+#Part C    
+import urllib2
+
+def currency_response(currency_from, currency_to, amount_from):
+    """Returns: a JSON string that is a response to a currency query.
+    A currency query converts amount_from money in currency currency_from 
+    to the currency currency_to. The response should be a string of the form
+    '{"from":"<old-amt>","to":"<new-amt>","success":true, "error":""}'
+    where the values old-amount and new-amount contain the value and name 
+    for the original and new currencies. If the query is invalid, both 
+    old-amount and new-amount will be empty, while "valid" will be followed 
+    by the value false.
+    Parameter currency_from: the currency on hand
+    Precondition: currency_from is a string
+    Parameter currency_to: the currency to convert to
+    Precondition: currency_to is a string
+    Parameter amount_from: amount of currency to convert
+    Precondition: amount_from is a float"""
+    
+    url = 'http://cs1110.cs.cornell.edu/2016fa/a1server.php?from=' + currency_from + '&to=' + currency_to + '&amt=' + str(amount_from)
+    webPage = urllib2.urlopen(url)
+    json = webPage.read()
+    return json
+
+
+#Part D
+def iscurrency(currency):
+    """Returns: True if currency is a valid (3 letter code for a) currency. 
+    It returns False otherwise.
+    Parameter currency: the currency code to verify
+    Precondition: currency is a string."""
+    
+    json = currency_response(currency, currency, 1)
+    return has_error(json) == False
    
-   #finds first double quote
- #   start=s.index('"')
-    #finds part after double quote
- #   tail=s[start+1:]
-    #finds second double quote
- #   end=tail.index('"')
-    #defines result as part the part within double quotes
- #   result=tail[:end]
-   #returns part of string within double quotes
- #   return result
-
-#def exchange(currency_from, currency_to, amount_from):
+def exchange(currency_from, currency_to, amount_from):
     """Returns: amount of currency received in the given exchange.
-
     In this exchange, the user is changing amount_from money in 
     currency currency_from to the currency currency_to. The value 
     returned represents the amount in currency currency_to.
-
     The value returned has type float.
-
     Parameter currency_from: the currency on hand
     Precondition: currency_from is a string for a valid currency code
-    
     Parameter currency_to: the currency to convert to
     Precondition: currency_to is a string for a valid currency code
-    
     Parameter amount_from: amount of currency to convert
     Precondition: amount_from is a float"""
+    
+    json = currency_response(currency_from, currency_to, amount_from)
+    convertedAmount = get_to(json)
+    convertedFloat = before_space(convertedAmount)
+    return float(convertedFloat)  
+    
--- a1test.py
+++ a1test.py
@@ -1,5 +1,5 @@
 #a1test.py
-#Emma Vedock-Gross ev225
+#Emma Vedock-Gross ev225 Trey Aguirre tea42
 #9/8/2016
 
 """Unit test for module a1
@@ -14,6 +14,7 @@ import a1
 #needs specification, test for part A
 def testA():
     #test cases for before_space function
+    
     result=a1.before_space('abc def')
     cornelltest.assert_equals('abc',result)
     
@@ -42,15 +43,103 @@ def testA():
     
     
 def testB():
-    pass
+    #test cases for first_inside_quotes
+    
+    result=a1.first_inside_quotes('a "bc" d')
+    cornelltest.assert_equals('bc',result)
+    
+    result=a1.first_inside_quotes('"ab" c')
+    cornelltest.assert_equals('ab',result)
+    
+    result=a1.first_inside_quotes('ab "cd" "de"')
+    cornelltest.assert_equals('cd',result)
+    
+    result=a1.first_inside_quotes('ab"cd"')
+    cornelltest.assert_equals('cd',result)
+                                  
+    
+    #test cases for get_from
+    result=a1.get_from('{ "from" : "2.5 United States Dollars", "to" : "2.24075 Euros", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('2.5 United States Dollars',result)
+    
+    result=a1.get_from('{ "from" : "40 Chinese Yuan", "to" : "23.864622009701 Romanian Lei", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('40 Chinese Yuan',result)
+    
+    result=a1.get_from('{ "from" : "1000 Zimbabwean Dollars", "to" : "26.611461135503 Swedish Kronor", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('1000 Zimbabwean Dollars',result)
+    
+    result=a1.get_from('{ "from" : "80 Gibraltar Pounds", "to" : "2349.5741654147 Ethiopian Birr", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('80 Gibraltar Pounds',result)
 
+    
+    #test cases for get_to
+    result=a1.get_to('{ "from" : "2.5 United States Dollars", "to" : "2.24075 Euros", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('2.24075 Euros',result)
+    
+    result=a1.get_to('{ "from" : "40 Chinese Yuan", "to" : "23.864622009701 Romanian Lei", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('23.864622009701 Romanian Lei',result)
+    
+    result=a1.get_to('{ "from" : "1000 Zimbabwean Dollars", "to" : "26.611461135503 Swedish Kronor", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('26.611461135503 Swedish Kronor',result)
+    
+    result=a1.get_to('{ "from" : "80 Gibraltar Pounds", "to" : "2349.5741654147 Ethiopian Birr", "success" : true, "error" : "" }')
+    cornelltest.assert_equals('2349.5741654147 Ethiopian Birr',result)
+    
+    
+    #test cases for has_error
+    result=a1.has_error('{"from":"","to":"","success":false,"error":"Source currency code is invalid."}')
+    cornelltest.assert_equals(True,result)
+    
+    result=a1.has_error('{ "from" : "2.5 United States Dollars", "to" : "2.24075 Euros", "success" : true, "error" : "" }')
+    cornelltest.assert_equals(False,result)
+    
+    result=a1.has_error('{ "from" : "80 Gibraltar Pounds", "to" : "2349.5741654147 Ethiopian Birr", "success" : true, "error" : "" }')
+    cornelltest.assert_equals(False,result)
+    
+    result=a1.has_error('{ "from" : "40 Chinese Yuan", "to" : "23.864622009701 Romanian Lei", "success" : true, "error" : "" }')
+    cornelltest.assert_equals(False,result)
+    
+    
+    
 def testC():
-    pass
+    #test procedures for currency_response function
+    
+    result = a1.currency_response('USD', 'EUR', 2.5)
+    cornelltest.assert_equals('{ "from" : "2.5 United States Dollars", "to" : "2.24075 Euros", "success" : true, "error" : "" }', result)
+    
+    result = a1.currency_response('CNY', 'RON', 40.0)
+    cornelltest.assert_equals('{ "from" : "40 Chinese Yuan", "to" : "23.864622009701 Romanian Lei", "success" : true, "error" : "" }', result)
+    
+    result = a1.currency_response('ZWL', 'SEK', 1000.0)
+    cornelltest.assert_equals('{ "from" : "1000 Zimbabwean Dollars", "to" : "26.611461135503 Swedish Kronor", "success" : true, "error" : "" }', result)
+    
+    result = a1.currency_response('GIP', 'ETB', 80.0)
+    cornelltest.assert_equals('{ "from" : "80 Gibraltar Pounds", "to" : "2349.5741654147 Ethiopian Birr", "success" : true, "error" : "" }', result)
+
 
 def testD():
-    pass
-#result = module.function()
-#cornelltest.assert_equals('",result)
+    #test procedures for iscurrency function
+    
+    cornelltest.assert_true(a1.iscurrency('USD'))
+    
+    cornelltest.assert_false(a1.iscurrency('123'))
+    
+    cornelltest.assert_true(a1.iscurrency('SEK'))
+    
+    cornelltest.assert_false(a1.iscurrency('ZZZ'))
+    
+    #test procedures for exchange function
+    result = a1.exchange('USD', 'EUR', 2.5)
+    cornelltest.assert_floats_equal(2.24075, result)
+    
+    result = a1.exchange('CNY', 'RON', 40.0)
+    cornelltest.assert_floats_equal(23.864622009701, result)
+    
+    result = a1.exchange('ZWL', 'SEK', 1000.0)
+    cornelltest.assert_floats_equal(26.611461135503, result)
+    
+    result = a1.exchange('GIP', 'ETB', 80.0)
+    cornelltest.assert_floats_equal(2349.5741654147, result)
 
 testA()
 testB()
